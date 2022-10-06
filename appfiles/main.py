07e142a01c8ba5c4a2b305c7bbb0e6fa88dc0dae
Katie Noble <katierebeccanoble@gmail.com>
2022-10-06 16:57:32 +0100

    Added hit counter

--- appfiles/main.py
+++ appfiles/main.py
@@ -7,7 +7,7 @@ import requests
 app_id = "8ecbd8b2"
 app_key = "40ff04984f164671df86c0247637c67b"
 
-# INGREDIENT SEARCH #
+# INGREDIENT SEARCH RESPONSE #
 
 ingredient = input("What ingredient do you need to use?")
 recipe_url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
@@ -15,6 +15,13 @@ recipe_url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(in
 response = requests.get(recipe_url)
 results = response.json()
 print(response)
+
+# HITS COUNTER #
+
+hits = results['hits']
+print("We found {} hits for your search that use {}!".format(str(len(hits)), ingredient))
+
+
 
 
 
